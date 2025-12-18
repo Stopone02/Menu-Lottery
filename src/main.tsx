8fa3b811c7a7c7f532bf7bfba25e89815331587f
stopone02 <jjo0522@catholic.ac.kr>
2025-12-18 23:14:21 +0900

    해시 라우터 추가
    - github pages에서 해시 라우터 없으면 404 뜨는 문제 해결.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'next-themes'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
@@ -12,11 +12,11 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider attribute="class" defaultTheme="light">
-        <BrowserRouter basename="/Menu-Lottery">
+        <HashRouter>
           <Routes>
             <Route path="/:code?" element={<App />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
